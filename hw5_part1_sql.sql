338d48253708a1d4c4ebba9517f032b735c41a9c
Unknown <stringhampreston@gmail.com>
2021-05-10 21:29:58 -0600

    Removed Exec for time being

--- hw5_part1_sql.sql
+++ hw5_part1_sql.sql
@@ -1,58 +1,66 @@
 -- Given CREATE TABLE statements to start your database
-CREATE PROCEDURE InitDataModel AS 
-BEGIN
-	Create Table Caregivers(
-		CaregiverId int IDENTITY PRIMARY KEY,
-		CaregiverName varchar(50)
-		);
-
-	Create Table AppointmentStatusCodes(
-		StatusCodeId int PRIMARY KEY,
-		StatusCode   varchar(30)
+Create Table Caregivers(
+	CaregiverId int IDENTITY PRIMARY KEY,
+	CaregiverName varchar(50)
 	);
 
-	Create Table Patients(
-		PatientId int IDENTITY PRIMARY KEY,
-		Name varchar(50),
-		dob DATE,
-		gender varchar(30),
-		sideeffects varchar(200)
-	)
+Create Table AppointmentStatusCodes(
+	StatusCodeId int PRIMARY KEY,
+	StatusCode   varchar(30)
+);
 
-	Create Table Vaccines(
-		VaccineId int IDENTITY PRIMARY KEY,
-		Name varchar(60),
-		inventory int DEFAULT 0 NOT NULL,
-		reserved int DEFAULT 0 NOT NULL
-	)
+Create Table Patients(
+	PatientId int IDENTITY PRIMARY KEY,
+	Name varchar(50),
+	dob DATE,
+	gender varchar(30),
+	sideeffects varchar(200)
+);
 
-	Create Table CareGiverSchedule(
-		CaregiverSlotSchedulingId int Identity PRIMARY KEY, 
-		CaregiverId int DEFAULT 0 NOT NULL
-			CONSTRAINT FK_CareGiverScheduleCaregiverId FOREIGN KEY (CaregiverId)
-				REFERENCES Caregivers(CaregiverId),
-		WorkDay date,
-		SlotTime time,
-		SlotHour int DEFAULT 0 NOT NULL,
-		SlotMinute int DEFAULT 0 NOT NULL,
-		SlotStatus int  DEFAULT 0 NOT NULL
-			CONSTRAINT FK_CaregiverStatusCode FOREIGN KEY (SlotStatus) 
-				REFERENCES AppointmentStatusCodes(StatusCodeId),
-		VaccineAppointmentId int DEFAULT 0 NOT NULL);
+Create Table Vaccines(
+	VaccineId int IDENTITY PRIMARY KEY,
+	Name varchar(60),
+	inventory int DEFAULT 0 NOT NULL,
+	reserved int DEFAULT 0 NOT NULL
+);
 
-	Create Table VaccineAppointment(
-		VaccineAppointmentId int IDENTITY PRIMARY KEY,
-		PatientId int NOT NULL
-			CONSTRAINT FK_PatientId FOREIGN KEY (PatientId)
-				REFERENCES Patients(PatientId),
-		firstshot bit,
-		VaccineId int NOT NULL
-			CONSTRAINT FK_VaccineId FOREIGN KEY (VaccineId)
-				REFERENCES Vaccines(VaccineId)
-	)
-END
 
-EXEC InitDataModel;
+INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+	VALUES (0, 'Open');
+INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+	VALUES (1, 'OnHold');
+INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+	VALUES (2, 'Scheduled');
+INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+	VALUES (3, 'Completed');
+INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+	VALUES (4, 'Missed');
+
+Create Table CareGiverSchedule(
+	CaregiverSlotSchedulingId int Identity PRIMARY KEY, 
+	CaregiverId int DEFAULT 0 NOT NULL
+		CONSTRAINT FK_CareGiverScheduleCaregiverId FOREIGN KEY (CaregiverId)
+			REFERENCES Caregivers(CaregiverId),
+	WorkDay date,
+	SlotTime time,
+	SlotHour int DEFAULT 0 NOT NULL,
+	SlotMinute int DEFAULT 0 NOT NULL,
+	SlotStatus int  DEFAULT 0 NOT NULL
+		CONSTRAINT FK_CaregiverStatusCode FOREIGN KEY (SlotStatus) 
+			REFERENCES AppointmentStatusCodes(StatusCodeId),
+	VaccineAppointmentId int DEFAULT 0 NOT NULL);
+
+Create Table VaccineAppointment(
+	VaccineAppointmentId int IDENTITY PRIMARY KEY,
+	PatientId int NOT NULL
+		CONSTRAINT FK_PatientId FOREIGN KEY (PatientId)
+			REFERENCES Patients(PatientId),
+	firstshot bit,
+	VaccineId int NOT NULL
+		CONSTRAINT FK_VaccineId FOREIGN KEY (VaccineId)
+			REFERENCES Vaccines(VaccineId)
+);
+
 -- Additional helper code for your use if needed
 
 -- --- Drop commands to restructure the DB
